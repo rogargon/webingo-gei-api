35729d130cb9713c00eb2600ea526fb37c12265f
Jordi Blanco Lopez <jordi14and@gmail.com>
2019-03-05 18:48:04 +0100

    Created Game class and Game Repository

--- src/main/java/cat/udl/eps/entsoftarch/webingogeiapi/repository/GameRepository.java
+++ src/main/java/cat/udl/eps/entsoftarch/webingogeiapi/repository/GameRepository.java
@@ -7,10 +7,4 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 @RepositoryRestResource
 public interface GameRepository extends PagingAndSortingRepository<Game, Integer> {
 
-  /**
-   * Finds the game with given id
-   * @param id Integer id
-   * @return Returns game object.
-   */
-  Game findByIdGame(Integer id);
 }
