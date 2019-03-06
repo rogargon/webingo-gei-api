f9e3445e3965d01a02e0102cc84613bfab6f16c1
Alex <satouralex@gmail.com>
2019-03-06 16:27:45 +0100

    Create Card Features and basic steps created

---
+++ src/test/java/cat/udl/eps/entsoftarch/webingogeiapi/steps/CardStepDef.java
@@ -0,0 +1,35 @@
+package cat.udl.eps.entsoftarch.webingogeiapi.steps;
+
+import cat.udl.eps.entsoftarch.webingogeiapi.WebingoGeiApiApplication;
+import cucumber.api.java.en.And;
+import cucumber.api.java.en.When;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootContextLoader;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@ContextConfiguration(
+        classes = {WebingoGeiApiApplication.class},
+        loader = SpringBootContextLoader.class
+)
+@DirtiesContext
+@RunWith(SpringRunner.class)
+@WebAppConfiguration
+@ActiveProfiles("Test")
+public class CardStepDef {
+    @When("^I create a new game with price (\\\\d+.\\\\d+)$")
+    public void I_create_a_new_game_with_price(double arg) {
+
+    }
+
+    @And("^I create a card$")
+    public void iCreateACard() {
+    }
+
+    @And("^A card has been created with price (\\\\d+.\\\\d+)")
+    public void aCardHasBeenCreatedWithPrice(int arg0, int arg1) {
+    }
+}
--- src/test/resources/features/CreateCard.feature
+++ src/test/resources/features/CreateCard.feature
@@ -2,3 +2,19 @@ Feature: Create Card
   In order to be able to play a Game
   As a player
   I want to be able to create a card
+
+Scenario: Generate a new card as a the player who created the game
+    Given I login as "player1" with password "password"
+    When I create a new game with price 10.0
+    And I create a card
+    Then  The response code is 201
+    And A card has been created with price 10.0
+
+Scenario: Generate a new card as another player
+  Given I login as "player1" with password "password"
+  And I create a new game with price 10.0
+  When I'm not logged in
+  And I login as "player2" with password "password"
+  And I create a card
+  Then  The response code is 201
+  And A card has been created with price 10.0
