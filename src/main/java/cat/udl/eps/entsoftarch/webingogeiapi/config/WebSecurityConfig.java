ba6d65caae9b682ae40f408f0d8ae6cc21b4da55
Lluis <llmas1996@mail.com>
2019-04-09 18:48:45 +0200

    Fix build

--- src/main/java/cat/udl/eps/entsoftarch/webingogeiapi/config/WebSecurityConfig.java
+++ src/main/java/cat/udl/eps/entsoftarch/webingogeiapi/config/WebSecurityConfig.java
@@ -32,13 +32,11 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         .antMatchers(HttpMethod.DELETE, "/admins*/**").hasRole("ADMIN")
 
         .antMatchers(HttpMethod.GET, "/players*/**").authenticated()
+        .antMatchers(HttpMethod.PUT, "/players*/**").authenticated()
         .antMatchers(HttpMethod.POST, "/players*/**").hasRole("ADMIN")
         .antMatchers(HttpMethod.DELETE, "/players*/**").hasRole("ADMIN")
 
         .antMatchers(HttpMethod.POST, "/invitations*/**").hasRole("PLAYER")
-            .antMatchers(HttpMethod.POST, "/invitations*/**").hasRole("ADMIN")
-            .antMatchers(HttpMethod.GET, "/**/*").authenticated()
-            .antMatchers(HttpMethod.POST, "/**/*").authenticated()
         .antMatchers(HttpMethod.PATCH, "/**/*").authenticated()
         .antMatchers(HttpMethod.DELETE, "/**/*").authenticated()
         .anyRequest().permitAll()
