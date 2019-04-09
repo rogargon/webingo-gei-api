950934245aac13118d3d2b6b997b6fb6d9c26aa2
Alvaro Ortega <alortegama@outlook.com>
2019-04-09 20:39:27 +0200

    added improvements in Invitation

--- src/main/java/cat/udl/eps/entsoftarch/webingogeiapi/domain/Invitation.java
+++ src/main/java/cat/udl/eps/entsoftarch/webingogeiapi/domain/Invitation.java
@@ -2,12 +2,7 @@ package cat.udl.eps.entsoftarch.webingogeiapi.domain;
 
 import com.fasterxml.jackson.annotation.JsonIdentityReference;
 import java.time.ZonedDateTime;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Lob;
-import javax.persistence.ManyToOne;
+import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 import lombok.Data;
@@ -22,6 +17,7 @@ public class Invitation {
     private long id;
 
     @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+    @Column(name = "created_at")
     private ZonedDateTime createdAt;
 
     @ManyToOne
@@ -30,14 +26,16 @@ public class Invitation {
 
     @ManyToOne
     @JsonIdentityReference(alwaysAsId = true)
+    @Column(name = "created_by")
     private Player createdBy;
 
     @ManyToOne
     @JsonIdentityReference(alwaysAsId = true)
+    @Column(name = "invites_to")
     private Game invitesTo;
 
     @Lob
-    @NotBlank
+    @NotBlank(message = "The message is mandatory")
     @Size(max = 255)
     private String message;
 }
