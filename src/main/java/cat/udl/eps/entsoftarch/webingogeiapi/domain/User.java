4e232d2fde7b86fa67cb335566809d46c3f02a36
Alvaro Ortega <alortegama@outlook.com>
2019-05-08 18:47:02 +0200

    User setEnabled Refactor CardStepDefs Refactor RegisterGameStepDefs

--- src/main/java/cat/udl/eps/entsoftarch/webingogeiapi/domain/User.java
+++ src/main/java/cat/udl/eps/entsoftarch/webingogeiapi/domain/User.java
@@ -1,6 +1,8 @@
 package cat.udl.eps.entsoftarch.webingogeiapi.domain;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
@@ -19,81 +21,88 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @Data
 @EqualsAndHashCode(callSuper = true)
 public abstract class User extends UriEntity<String> implements UserDetails {
+	/**
+	 * Creates a new instance of password encoder.
+	 */
+	public static PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+	/**
+	 * Identifier of user needs to be unique, otherwise it will generate conflicts.
+	 */
+	@Id
+	private String username;
+	/**
+	 * Email of user needs to be unique, otherwise it will generate conflicts and it can not be blank.
+	 */
+	@NotBlank
+	@Email
+	private String email;
+	/**
+	 * Password of user needs to be in range of 8 to 256 characters, and it can not be blank
+	 * Access to this Json property is in write only mode.
+	 */
+	@JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+	@NotBlank
+	@Length(min = 8, max = 256)
+	private String password;
 
-  /**
-   *
-   * Creates a new instance of password encoder.
-   */
+	private boolean enabled =true;
 
-  public static PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+	@Override
+	public String getId() {
+		return username;
+	}
 
-  /**
-   * Identifier of user needs to be unique, otherwise it will generate conflicts.
-   */
-  @Id
-  private String username;
+	@Override
+	public String getUsername() {
+		return username;
+	}
 
+	@Override
+	public String getPassword() {
+		return password;
+	}
 
-  /**
-   * Email of user needs to be unique, otherwise it will generate conflicts and it can not be blank.
-   */
-  @NotBlank @Email
-  private String email;
+	public void encodePassword() {
+		this.password = passwordEncoder.encode(this.password);
+	}
 
-  /**
-   * Password of user needs to be in range of 8 to 256 characters, and it can not be blank
-   * Access to this Json property is in write only mode.
-   */
+	/**
+	 * Checks if Account has not expired.
+	 *
+	 * @return true.
+	 */
+	@Override
+	public boolean isAccountNonExpired() {
+		return true;
+	}
 
-  @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
-  @NotBlank @Length(min=8, max=256)
-  private String password;
+	/**
+	 * Checks if Account is not locked.
+	 *
+	 * @return true.
+	 */
+	@Override
+	public boolean isAccountNonLocked() {
+		return true;
+	}
 
+	/**
+	 * Checks if credentials are not expired.
+	 *
+	 * @return true.
+	 */
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return true;
+	}
 
-  @Override
-  public String getId() {
-    return username;
-  }
-
-  @Override
-  public String getUsername(){
-    return username;
-  }
-
-  @Override
-  public String getPassword() {
-    return password;
-  }
-
-  public void encodePassword() {
-    this.password = passwordEncoder.encode(this.password);
-  }
-
-  /**
-   * Checks if Account has not expired.
-   * @return true.
-   */
-  @Override
-  public boolean isAccountNonExpired() { return true; }
-
-  /**
-   * Checks if Account is not locked.
-   * @return true.
-   */
-  @Override
-  public boolean isAccountNonLocked() { return true; }
-
-  /**
-   * Checks if credentials are not expired.
-   * @return true.
-   */
-  @Override
-  public boolean isCredentialsNonExpired() { return true; }
-
-  /**
-   * Checks if Account is enabled.
-   * @return true.
-   */
-  @Override
-  public boolean isEnabled() { return true; }
+	/**
+	 * Checks if Account is enabled.
+	 *
+	 * @return true.
+	 */
+	@Override
+	public boolean isEnabled() {
+		return enabled;
+	}
 }
