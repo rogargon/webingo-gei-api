934d706938fce28a717a296c00c932fff96f8dff
Lluis <16152037+LluisMas@users.noreply.github.com>
2019-05-16 15:23:53 +0200

    Allow paging for findByInvites and findByCreatedBy

--- src/main/java/cat/udl/eps/entsoftarch/webingogeiapi/repository/InvitationRepository.java
+++ src/main/java/cat/udl/eps/entsoftarch/webingogeiapi/repository/InvitationRepository.java
@@ -1,18 +1,17 @@
 package cat.udl.eps.entsoftarch.webingogeiapi.repository;
 
 
-import cat.udl.eps.entsoftarch.webingogeiapi.domain.Game;
 import cat.udl.eps.entsoftarch.webingogeiapi.domain.Invitation;
 import cat.udl.eps.entsoftarch.webingogeiapi.domain.Player;
 import cat.udl.eps.entsoftarch.webingogeiapi.domain.User;
-import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.security.access.prepost.PreAuthorize;
 
 import java.util.List;
-import java.util.Optional;
 
 @RepositoryRestResource
 public interface InvitationRepository extends PagingAndSortingRepository<Invitation, Long>  {
@@ -21,10 +20,8 @@ public interface InvitationRepository extends PagingAndSortingRepository<Invitat
     List<Invitation> findById(@Param("id") long id);
 
     @PreAuthorize("#invites.username == principal.username")
-    List<Invitation> findByInvites(@Param("invites") User invites);
+    Page<Invitation> findByInvites(@Param("invites") User invites, Pageable p);
 
     @PreAuthorize("#createdBy.username == principal.username")
-    List<Invitation> findByCreatedBy(@Param("createdBy") Player createdBy);
-
-
+    Page<Invitation> findByCreatedBy(@Param("createdBy") Player createdBy, Pageable p);
 }
