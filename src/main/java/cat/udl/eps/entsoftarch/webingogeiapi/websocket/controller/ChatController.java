80356da80e751feeb42b6630560ab7c928b8e372
Lluis <16152037+LluisMas@users.noreply.github.com>
2019-06-06 10:45:32 +0200

    schedule messages socket

--- src/main/java/cat/udl/eps/entsoftarch/webingogeiapi/websocket/controller/ChatController.java
+++ src/main/java/cat/udl/eps/entsoftarch/webingogeiapi/websocket/controller/ChatController.java
@@ -7,8 +7,11 @@ import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Controller;
 
+@EnableScheduling
 @Controller
 public class ChatController {
 
@@ -27,17 +30,21 @@ public class ChatController {
 
     @MessageMapping("/chat.addUser")
     @SendTo("/topic/public")
-    public ChatMessage addUser(@Payload ChatMessage chatMessage,
-                               SimpMessageHeaderAccessor headerAccessor) {
+    public ChatMessage addUser(@Payload ChatMessage chatMessage, SimpMessageHeaderAccessor headerAccessor) {
         // Add username in web socket session
         headerAccessor.getSessionAttributes().put("username", chatMessage.getSender());
-        this.template.convertAndSend("/topic/public", "vergoooo");
+        this.template.convertAndSend("/topic/public", "adduser");
         return chatMessage;
     }
 
     @MessageMapping("/send/message")
     public void onReceivedMessage(String message) {
-        this.template.convertAndSend("/topic/public", "vergoooo");
+        this.template.convertAndSend("/topic/public", "onreceive");
+    }
+
+    @Scheduled(fixedRate = 5000)
+    public void greeting() {
+        this.template.convertAndSend("/topic/public", "Hello");
     }
 
 }
